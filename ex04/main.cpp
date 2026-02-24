3d0d874c1e23725fc0f9521241ed192ab017e287
ayse-nur-gedikoglu <gedikoglu_27@icloud.com>
2026-02-24 23:35:43 +0300

    sanırım hasta oldum

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,30 +6,53 @@ int main(int argc, char **argv)
 {
     if (argc != 4)
     {
-        std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <filename> <from> <to>" << std::endl;
         return 1;
     }
-    std::string filename = argv[1];
-    std::string s1 = argv[2];
-    std::string s2 = argv[3];
-
-    std::ifstream infile("s1"); //okuma
-    if (!infile)
+    std::string file = argv[1];
+    std::string from = argv[2]; //bu stringin yerine to konacak
+    std::string to = argv[3];
+    if (from.empty())
     {
-        std::cerr << "Error: could not open input file." << std::endl;
+        std::cerr << "Error: search string cannot be empty." << std::endl;
         return 2;
     }
-    std::ofstream outfile("s2"); //oluşturma ve yazma
-    if (!outfile)
+    std::ifstream src(file.c_str()); //okuma
+    if (!src)
     {
-        std::cerr << "Error: could not create output file." << std::endl;
+        std::cerr << "Error: could not open input file." << std::endl;
         return 3;
     }
-
-
-    
+    std::ofstream dest(file + ".replace"); //oluşturma ve yazma
+    if (!dest)
+    {
+        std::cerr << "Error: could not create output file." << std::endl;
+        return 4;
+    }
+    size_t pos;
+    std::string line;
+    while (std::getline(src, line)) //nereden okuyacak, nereye yazacak
+    {
+        pos = 0;
+        while ((pos = line.find(from, pos)) != std::string::npos)
+        {
+            line.erase(pos, from.length());
+            line.insert(pos, to);
+            pos += to.length();
+        }
+        dest << line << std::endl;
+    }
+    src.close();
+    dest.close();
+    return 0;
 }
 //3 parametre alacak
-//./file_replace <filename> <s1> <s2>
-//s1 yerine s2 koyacak
+//./file_replace <file> <string1> <string2>
+//string1 yerine string2 koyacak
 //`std::string` sınıfının tüm üye fonksiyonlarına izin verilir, `replace` fonksiyonu hariç.
+
+//eğer ofstream olan bir dosya adına calıstırılırsa üstüne yazar.
+//std::getline dosyadan (veya herhangi bir stream'den) 
+//bir satır okur ve onu bir string'e atar.
+
+//find aradıgı seyi bulmazsa npos döner. bulubamadı deme
