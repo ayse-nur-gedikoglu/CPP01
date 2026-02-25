fdca677c71dd5b0f0dea92314ae0ef7f1f0567a5
ayse-nur-gedikoglu <gedikoglu_27@icloud.com>
2026-02-25 06:29:42 +0300

    ex01 finish

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -1,1 +1,35 @@
 #include "Harl.hpp"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <DEBUG|INFO|WARNING|ERROR>" << std::endl;
+        return 0;
+    }
+    Harl harl;
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    int i = 0;
+    while (i < 4)
+    {
+        if (levels[i] == argv[1]) 
+            break;
+        i++;
+    }
+    switch (i)
+    {
+        case 0:
+            harl.complain("DEBUG");
+        case 1:
+            harl.complain("INFO");
+        case 2:
+            harl.complain("WARNING");
+        case 3:
+            harl.complain("ERROR");
+            break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    }
+    return 0;
+}
